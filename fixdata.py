119410f1a95ab0587d0c310cfda01d43a7c44613
umeshjoshi62 <umeshjoshi62@yahoo.in>
2023-05-02 16:13:12 -0500

    Update fixdata.py

--- fixdata.py
+++ fixdata.py
@@ -1,56 +1,54 @@
 import sys
 import csv
 
-maxInt = sys.maxsize
+def set_max_field_size():
+    maxInt = sys.maxsize
+    while True:
+        try:
+            csv.field_size_limit(maxInt)
+            break
+        except OverflowError:
+            maxInt = int(maxInt/10)
 
-while True:
-    # decrease the maxInt value by factor 10 
-    # as long as the OverflowError occurs.
+def fix_comment(row):
+    no_of_columns = 42
+    comment_col_ind = 18
+    if len(row) > no_of_columns:
+        no_of_cols_with_comment = len(row) - no_of_columns
+        comment_text = ' '.join([row[comment_col_ind+i] for i in range(no_of_cols_with_comment)])
+        
+        for i in range(no_of_cols_with_comment-1):
+            row.pop(comment_col_ind)
+        row[comment_col_ind] = comment_text
+    row[comment_col_ind] = row[comment_col_ind].replace('\t', '')
+    return row
 
-    try:
-        csv.field_size_limit(maxInt)
-        break
-    except OverflowError:
-        maxInt = int(maxInt/10)
-
-
-count_error=0
-
-
-# open the input file
-with open('input_file.txt', 'r') as f_in:
-    # create a CSV reader with tab delimiter
-    reader = csv.reader(f_in, delimiter='\t')
-    # open the output file
-    with open('output_file.txt', 'w',newline='') as f_out:
-        # create a CSV writer with tab delimiter
+def main():
+    max_row_to_scan = 100
+    no_of_column = (len(row) for i_row, row in enumerate(csv.reader(open('input_file.txt', 'r'), delimiter='\t')) if i_row <= max_row_to_scan)
+    max_column_no = sum(no_of_column) / max_row_to_scan
+    count_error = 0
+    with open('input_file.txt', 'r') as f_in, open('output_file.txt', 'w', newline='') as f_out:
+        reader = csv.reader(f_in, delimiter='\t')
         writer = csv.writer(f_out, delimiter='\t')
-        # iterate over each row in the input file
-        tmp_row_data=[]
+        tmp_row_data = []
         for row in reader:
-        # if the row has more than the expected number of fields
-            #print(len(row))
-            #print(row)
-            if len(row) > 40:
-                
-                if tmp_row_data :
-                    #print(tmp_row_data )
-                    
-                    row[-1] = tmp_row_data[-1] + row[0] 
-                    if(len(row)>0) :
-                        row.pop(0)
-                    row = row + tmp_row_data
-                    #print( row)
-                    writer.writerow(tmp_row_data + row) 
-                    tmp_row_data=[]
-                else:
-                    writer.writerow(row)
-
-                
+            if len(row) == 0:
+                continue
+            if len(row) + len(tmp_row_data) < max_column_no:
+                if len(row) > 0:
+                    count_error += 1
+                    tmp_row_data += row
             else:
-                if len(row) > 0 :
-                    count_error = count_error + 1
-                    tmp_row_data= tmp_row_data + row
-                # else:
-                #     writer.writerow(row)
-print("total errors : {}".format(count_error))
+                if tmp_row_data:
+                    row = tmp_row_data + row
+                    row = fix_comment(row)
+                    writer.writerow(row)
+                    tmp_row_data = []
+                else:
+                    row = fix_comment(row)
+                    writer.writerow(row)
+    print("total errors : {}".format(count_error))
+
+set_max_field_size()
+main()
